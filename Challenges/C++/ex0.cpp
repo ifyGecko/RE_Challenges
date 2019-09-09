285406a4cdd6fa6c2add9472fead847382b1cab1
ifyGecko <26214995+ifyGecko@users.noreply.github.com>
2019-09-09 12:30:59 -0500

    changed endl to \n

--- Challenges/C++/ex0.cpp
+++ Challenges/C++/ex0.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int main(int argc, char** argv){
 	if(argc!=2){
-		cout << "Usage: ./prog input" << endl;
-		cout << "Hint: ./prog -h" << endl;
+		cout << "Usage: ./prog input\n";
+		cout << "Hint: ./prog -h\n";
 	}else if(string(argv[1]) == "-h"){
-		cout << "The desired input is in plain text." << endl;
+		cout << "The desired input is in plain text.\n";
 	}else{
-		string(argv[1])=="password"?cout << "Score!" << endl:cout << "Fail!" << endl;
+		string(argv[1])=="password"?cout << "Score!\n":cout << "Fail!\n";
 	}
 	return 0;
 }
