2661c19850eca3c8e19c14ea446825edecd5e220
ifyGecko <26214995+ifyGecko@users.noreply.github.com>
2019-09-09 07:35:55 -0500

    changed printf to cout

--- Challenges/C++/ex6.cpp
+++ Challenges/C++/ex6.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int main(int argc, char** argv){
   if(argc!=2){
-    printf("Usage: ./prog input\n");
-    printf("Hint: ./prog -h\n");
+    cout << "Usage: ./prog input\n";
+    cout << "Hint: ./prog -h\n";
   }else if(argv[1]==string("-h")){
-    printf("Symbols are the mightiest of all human creations but, we can do with out.\n");
+    cout << "Symbols are the mightiest of all human creations but, we can do with out.\n";
   }else{
-    argv[1]==string("&&&&&&")?printf("Score!\n"):printf("Fail!\n");
+    argv[1]==string("&&&&&&")?cout << "Score!\n":cout << "Fail!\n";
   }
   return 0;
 }
