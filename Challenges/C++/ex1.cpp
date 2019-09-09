c09895ba9c2a2928ce92e215cdab13d12705bf68
ifyGecko <26214995+ifyGecko@users.noreply.github.com>
2019-09-09 12:31:49 -0500

    changed endl to \n

--- Challenges/C++/ex1.cpp
+++ Challenges/C++/ex1.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 int main(int argc, char** argv){
     	if(argc == 2 && string(argv[1]) == "-h"){
-		cout << "Hunt for arithmetic done to the argument vector elements." << endl;
+		cout << "Hunt for arithmetic done to the argument vector elements.\n";
     	}else if(argc!=3){
-		cout << "Usage: ./prog input" << endl;
-		cout << "Hint: ./prog -h" << endl;
+		cout << "Usage: ./prog input\n";
+		cout << "Hint: ./prog -h\n";
 	}else{
-		atoi(argv[1])+atoi(argv[2])==1950?cout << "Score!" << endl:cout << "Fail!" << endl;
+		atoi(argv[1])+atoi(argv[2])==1950?cout << "Score!\n":cout << "Fail!\n";
 	}
 	return 0;
 }
