3adc78cf315c940c149053556afa567942b4fc87
ifyGecko <26214995+ifyGecko@users.noreply.github.com>
2019-09-08 22:27:40 -0500

    added commenting

--- update.sh
+++ update.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+#update list of packages && upgrade installed packages
 apt update && apt upgrade -y
 
+#remove unneeded packages
 apt autoremove -y
 
+#update radare2 package manager
 r2pm update
 
+#clone new RE_Challenges repo to a temporary directory
 git clone https://github.com/ifyGecko/RE_Challenges tmp
 
+#update all files related to C challenges
 cd ~/RE_Challenges/tmp/Challenges/C
 make all
 mv *.c ~/RE_Challenges/Help/C/Source-Code
@@ -16,6 +21,7 @@ mv makefile ~/RE_Challenges/Help/C/Source-Code
 cd ~/RE_Challenges/tmp/Help/C
 mv *.docx ~/RE_Challenges/Help/C
 
+#update all files related to C++ challenges
 cd ~/RE_Challenges/tmp/Challenges/C++
 make all
 mv *.cpp ~/RE_Challenges/Help/C++/Source-Code
@@ -24,6 +30,7 @@ mv makefile ~/RE_Challenges/Help/C++/Source-Code
 cd ~/RE_Challenges/tmp/Help/C++
 mv *.docx ~/RE_Challenges/Help/C++
 
+#update all files related to Go challenges
 cd ~/RE_Challenges/tmp/Challenges/Go
 make all
 mv *.go ~/RE_Challenges/Help/Go/Source-Code
@@ -32,11 +39,14 @@ mv makefile ~/RE_Challenges/Help/Go/Source-Code
 cd ~/RE_Challenges/tmp/Help/Go
 mv *.docx ~/RE_Challenges/Help/Go
 
+#takes all lines that are new in the updated set-up.sh and runs them (ensure new tools will be installed)
 awk 'FNR==NR{a[$0];next}!($0 in a)' ~/RE_Challenges/set-up.sh ~/RE_Challenges/tmp/set-up.sh | /bin/bash
 
+#copy all updated scripts and README.md
 mv ~/RE_Challenges/tmp/set-up.sh ~/RE_Challenges
 mv ~/RE_Challenges/tmp/update.sh ~/RE_Challenges
 mv ~/RE_Challenges/tmp/remove.sh ~/RE_Challenges
 mv ~/RE_Challenges/tmp/README.md ~/RE_Challenges
 
+#remove tmp directory when the update is complete
 rm -rf ~/RE_Challenges/tmp
