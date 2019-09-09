8140344c8f51496a125aef661b178e279222240c
ifyGecko <26214995+ifyGecko@users.noreply.github.com>
2019-09-08 22:31:23 -0500

    added commenting and removal of cutter

--- remove.sh
+++ remove.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
+#remove radare2 packages
 r2pm -u r2dec
-
 r2pm -u r2ghidra-dec
 
+#remove packages installed from kali repos
 apt remove gccgo pkg-config libssl-dev bc libreoffice python3-venv cmake bison -y
 
+#remove all packages that remain that are no longer needed
 apt autoremove -y
 
+#remove all manually installed tools
 cd ~/
-
+rm /bin/cutter
 rm -rf ~/ghidra_9.0.4
-
 rm -rf ~/GdbShellPipe
-
 rm -rf ~/radare2
-
 rm -rf ~/peda
-
 rm -rf ~/RE_Challenges
