cc552fda200cc558eb3e916fa974375a57731853
ifyGecko <26214995+ifyGecko@users.noreply.github.com>
2019-09-15 16:44:15 -0500

    edited to indicate two inputs are required

--- Challenges/C++/ex1.cpp
+++ Challenges/C++/ex1.cpp
@@ -7,7 +7,7 @@ int main(int argc, char** argv){
     	if(argc == 2 && string(argv[1]) == "-h"){
 		cout << "Hunt for arithmetic done to the argument vector elements.\n";
     	}else if(argc!=3){
-		cout << "Usage: ./prog input\n";
+		cout << "Usage: ./prog input input\n";
 		cout << "Hint: ./prog -h\n";
 	}else{
 		atoi(argv[1])+atoi(argv[2])==1950?cout << "Score!\n":cout << "Fail!\n";
